7ae41575046cf65358958382115d37badc9563c8
Piotr Głuszek <piogluszek@gmail.com>
2018-04-08 13:21:35 +0200

    Add TextMeshPro and menu demo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public void StartSimulation()
+    {
+        Debug.Log("Simulation started.");
+    }
+
+    public void ShowOptions()
+    {
+        Debug.Log("Options shown.");
+    }
+
+    public void RunMapCreator()
+    {
+        Debug.Log("Map creator opened.");
+    }
+    public void Quit()
+    {
+        Debug.Log("Game closed.");
+        Application.Quit();
+    }
+}
