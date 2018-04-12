5cad90f4a0d176dd751843f3295b8c04b70e6c6f
Piotr Głuszek <piogluszek@gmail.com>
2018-04-12 12:09:23 +0200

    Add settings info to the Game

---
+++ Assets/SettingsVisualization.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SettingsVisualization : MonoBehaviour
+{
+
+    private TMP_Text _settings;
+    // Use this for initialization
+    void Start()
+    {
+        _settings = GetComponent<TMP_Text>();
+        _settings.SetText("Gen size: " + Settings.AttemptNum
+                          + "\nMutate rate: " + Settings.MutationRate
+                          + "\nCrossover rate: " + Settings.CrossoverRate
+                          + "\nMap: " + Settings.SelectedMapPath
+                          + "\nDNA: " + Settings.DnaFilePath);
+    }
+
+
+}
